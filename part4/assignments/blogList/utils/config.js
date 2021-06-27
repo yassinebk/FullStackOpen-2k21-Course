11fe4a5c66bcc43907619ee34f71b5c2efddb4ba
yassinebk <yassinebk23@gmail.com>
2021-06-27 10:10:03 +0100

    done

--- part4/assignments/blogList/utils/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const { PORT } = process.env;
-const { MONGODB_URI } = process.env;
-
-module.exports = {
-  MONGODB_URI,
-  PORT,
-};
--- part4/assignments/blogList/utils/for_testing.js
+++
@@ -1,8 +0,0 @@
-const palindrome = (string) => string.split("").reverse().join("");
-
-const average = (array) => {
-  const reducer = (sum, item) => sum + item;
-  return array.reduce(reducer, 0);
-};
-
-module.exports = { palindrome, average };
